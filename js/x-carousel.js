a599e56bf8742bd99d6de32c72153e988bab59df
Tatiana Liatambur <T.Goroshina@yandex.ru>
2017-03-27 01:56:30 +0300

    added hw_11-12

---
+++ js/x-carousel.js
@@ -0,0 +1,32 @@
+$ (function () {
+    var leftArrow = $('.carousel_control_prev');
+    var rightArrow = $('.carousel_control_next');
+    var elementsList =$('.carousel_list');
+
+    var picNumber = 0;
+    var picsCount = elementsList.find('li').length;
+
+    function showPicture(num) {
+        var offset = -644*num;
+        elementsList.animate({left : offset + "px"}, 500);
+    }
+
+
+
+    leftArrow.on('click', function () {
+        if (picNumber <= 0)
+            return;
+
+        picNumber--;
+        showPicture(picNumber);
+    });
+
+    rightArrow.on('click', function () {
+        picNumber++;
+
+        if (picNumber >= picsCount)
+            picNumber = 0;
+
+        showPicture(picNumber);
+    });
+});
